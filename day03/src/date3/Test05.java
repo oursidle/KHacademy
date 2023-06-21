f0c185cf85d7dd283da858e2b2af24425c7a07fd
oursidle <135007715+oursidle@users.noreply.github.com>
2023-06-21 21:51:18 +0900

    Create Test05.java
    논리 연산자를 활용해 건강검진 프로그램 만들어보기

---
+++ day03/src/date3/Test05.java
@@ -0,0 +1,34 @@
+package date3;
+
+public class Test05 {
+	public static void main(String[] args) {
+		//결론
+		//=30세 이상 && (짝수 해이면서 짝수 년생 || 홀수 해이면서 홀수 년생)
+		//= condition1 && (condition2 || condition3)
+		
+		//입력
+		int birth = 1985;
+		int year = 2023;
+		
+		//계산
+		//boolean condition1 = 30세 이상인가?;
+		//boolean condition2 = 짝수년도 && 짝수년생;
+		//boolean condition3 = 홀수년도 && 홀수년생;
+		//=boolean condition = (condition1 && condition2 || condition3);
+		int age = year - birth + 1;
+		boolean condition1 = age >= 30;
+		
+		boolean condition2Year = year % 2 == 0;
+		boolean condition2Birth = birth % 2 == 0;
+		boolean condition2 = condition2Year && condition2Birth;
+		
+		boolean condition3Year = year % 2 != 0;
+		boolean condition3Birth = birth % 2 != 0;
+		boolean condition3 = condition3Year && condition3Birth;
+		
+		boolean condition = (condition1 && condition2 || condition3);
+		
+		//출력		
+		System.out.println(condition);
+	}
+}
