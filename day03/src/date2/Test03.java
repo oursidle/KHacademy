caa9581a4080e7316c8e3d49e8cc5471537b20a0
ourskim <ourskim@naver.com>
2023-06-21 13:37:55 +0900

    타입 캐스팅을 활용한 나눗셈 계산
    타입 캐스팅을 활용해 주유소 프로그램을 만들어보기

---
+++ day03/src/date2/Test03.java
@@ -0,0 +1,15 @@
+package date2;
+
+public class Test03 {
+	public static void main(String[] args) {
+		//입력
+		int unitPrice = 1530 ;
+		int money = 50000;
+		
+		//계산
+		double liter = (double)money / unitPrice;
+		
+		//출력
+		System.out.println(liter);
+	}
+}
