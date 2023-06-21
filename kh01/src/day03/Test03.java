48cccd0ef11d2871c17c12824d9e7c7627ab5f84
hyoli <hyoli@DESKTOP-B9IRVAV>
2023-06-21 20:41:04 +0900

    논리형을 활용한 짝수 계산
    논리형 boolean을 사용해 짝수 판정 프로그램 만들어보기

---
+++ kh01/src/day03/Test03.java
@@ -0,0 +1,16 @@
+package day03;
+
+public class Test03 {
+	public static void main(String[] args) {
+		//짝수 판정
+		
+		//입력
+		int number = 17;
+		
+		//계산
+		boolean zero = number %  2 == 0;
+		
+		//출력
+		System.out.println(zero);
+	}
+}
