775f26134751459a0f3324c75bff08c8a50040c1
ourskim <ourskim@naver.com>
2023-06-23 15:17:33 +0900

    switch-case 구문을 활용한 사계절 출력

--- day05/src/condition3/Test02.java
+++ day05/src/condition3/Test02.java
@@ -9,19 +9,13 @@ public class Test02 {
 		
 		
 		switch(month) {//month 에 있는 값을 토대로 실행 지점을 찾겠다
-		case 12:
-		case 1: 
-		case 2: //month 가 2인 경우 실행할 곳
+		case 12, 1, 2: //month 가 2인 경우 실행할 곳
 			System.out.print("겨울");
 			break;
-		case 3: //month 가 3인 경우 실행할 곳
-		case 4:
-		case 5:
+		case 3, 4, 5://month 가 3인 경우 실행할 곳
 			System.out.println('봄');
 			break;
-		case 6:
-		case 7:
-		case 8:
+		case 6, 7, 8:
 			System.out.println("여름");
 			break;
 		default:
