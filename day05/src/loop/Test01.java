03e0ce9474bce85076b6a37616557812fe03b7a3
ourskim <ourskim@naver.com>
2023-06-23 17:28:25 +0900

    for 반복문 개념

---
+++ day05/src/loop/Test01.java
@@ -0,0 +1,16 @@
+package loop;
+
+public class Test01 {
+	public static void main(String[] args) {
+		//반복(loop)
+		//Q: Hello를 화면에 10번 출력하세요
+		
+		//for(int i=0;  i < 10; i = i + 1) {
+		//for(int i=0;  i < 10; i += 1) {
+		//for(int i=1;  i <= 10; i++) {//위 세 가지 방법 모두 맞지만, 잘 쓰이지 않음
+		for(int i=0;  i < 10; i++) {
+			}
+			System.out.println("Hello");
+		
+	}
+}
