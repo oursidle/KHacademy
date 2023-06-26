def7acd829dda49f23e715478542f0d3192d125b
ourskim <ourskim@naver.com>
2023-06-26 09:44:40 +0900

    1~99 사이의 5가 포함된 숫자 추출

---
+++ day05/src/loop/Test04.java
@@ -0,0 +1,16 @@
+package loop;
+
+public class Test04 {
+	public static void main(String[] args) {
+		
+		for(int i=1; i <= 99; i++) {
+			boolean ten = i / 10 == 5;
+			boolean one = i % 10 == 5;
+			boolean five= ten || one;
+			if(five) {
+				System.out.println("i = " + i);
+			}else {
+			}
+		}
+	}
+}
