10fd6b01825eabd0ffcce56d38dcb15cc71a75b5
ourskim <ourskim@naver.com>
2023-06-27 09:48:43 +0900

    반복문을 활용한 공부 시간 합계, 평균 구하기

--- day06/src/loop2/Test10.java
+++ day06/src/loop2/Test10.java
@@ -5,11 +5,15 @@ import java.util.Scanner;
 public class Test10 {
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
 		int total = 0;
 		int count = 0;
 		
-		Scanner sc = new Scanner(System.in);
-		for(int i=1; i <= 5; i++) {
+		System.out.print("공부한 일수 입력: ");
+		int day = sc.nextInt();
+		
+		for(int i=1; i <= day; i++) {
 			System.out.print(i + "일차 공부(시간): ");
 			int hour = sc.nextInt();
 			System.out.print(i + "일차 공부(분): ");
