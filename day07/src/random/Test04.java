2ee62771c45870ec2d404ebd51d2e384eb7d039e
ourskim <ourskim@naver.com>
2023-06-27 13:42:12 +0900

    모두의 마블 룰의 주사위 게임 프로그램

---
+++ day07/src/random/Test04.java
@@ -0,0 +1,29 @@
+package random;
+
+import java.util.Random;
+
+public class Test04 {
+	public static void main(String[] args) {
+		
+		Random r = new Random();
+		
+		int size = 100;
+		int count = 0;
+		
+		for(int i=0; i < size; i++) {
+			int dice1 = r.nextInt(6) + 1;
+			int dice2 = r.nextInt(6) + 1;
+			
+			if(dice1 == dice2) {
+				count ++;
+				dice1 = r.nextInt(6) + 1;
+				dice2 = r.nextInt(6) + 1;
+			}else {}
+		}
+		System.out.println("**더블 개수: " + count);
+		
+		double avg = (double)count / size * 100.0;
+		
+		System.out.println("**더블 확률: " + avg + " %");
+	}
+}
