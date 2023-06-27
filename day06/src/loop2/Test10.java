7b85edb6c1ddd9ceb2166f4782f8b7e73c1da5cf
ourskim <ourskim@naver.com>
2023-06-27 09:44:41 +0900

    반복문을 활용한 시간 합계, 평균 구하기

---
+++ day06/src/loop2/Test10.java
@@ -0,0 +1,36 @@
+package loop2;
+
+import java.util.Scanner;
+
+public class Test10 {
+	public static void main(String[] args) {
+		
+		int total = 0;
+		int count = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		for(int i=1; i <= 5; i++) {
+			System.out.print(i + "일차 공부(시간): ");
+			int hour = sc.nextInt();
+			System.out.print(i + "일차 공부(분): ");
+			int minute = sc.nextInt();
+			
+			int time = hour * 60 + minute;
+			total += time;
+			
+			count ++;
+		}
+		sc.close();
+		
+		int hour = total / 60;
+		int minute = total % 60;
+		
+		int avg = total / count;
+		int hourAvg = avg / 60;
+		int minAvg = avg % 60;
+	
+		
+		System.out.println("총 공부 시간: " + hour + "시간 " + minute + "분");
+		System.out.println("일 평균 공부 시간: 약 " + hourAvg + "시간 " + minAvg + "분");
+	}
+}
