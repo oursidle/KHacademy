f98a56df6600bddb5c0619168f841802b8d0667e
ourskim <ourskim@naver.com>
2023-06-28 10:14:59 +0900

    String 배열 만들기

---
+++ day08/src/array/Test04.java
@@ -0,0 +1,16 @@
+package array;
+
+public class Test04 {
+	public static void main(String[] args) {
+		
+		String[] s = new String[3];
+		
+		s[0] = "자바";
+		s[1] = "파이썬";
+		s[2] = "루비";
+		
+		System.out.println(s[0]);
+		System.out.println(s[1]);
+		System.out.println(s[2]);
+	}
+}
