9393209418c8adc6eba7b19aa5c36ae71ffcd51a
ourskim <ourskim@naver.com>
2023-06-28 16:39:53 +0900

    배열을 활용한 최대값과 그 위치 구하기

--- day08/src/array/Test10.java
+++ day08/src/array/Test10.java
@@ -11,14 +11,13 @@ public class Test10 {
 		//최대값은 [0] 위치가 가장 크다고 가정하고
 		//[1] 부터 끝까지 비교하여 더 큰 데이터가 나오면 덮어쓰기하여 구한다
 		
-		int maxIndex = 0;//0번 위치가 가장 크다고 가정
+		int max = data[0];
 		for(int i=1; i < data.length; i++) {
-			if(data[maxIndex] < data[i]) {//최대값보다 더 큰 숫자가 나온다면
-				maxIndex = i;//max의 값의 위치를 갈아엎음
+			if(max < data[i]) {//최대값보다 더 큰 숫자가 나온다면
+				max = data[i];//max의 값을 갈아엎음
 			}
 		}
 		
-		System.out.println("최대값 위치 = " + maxIndex);
-		System.out.println("최대값 = " + data[maxIndex]);
+		System.out.println("최대값 = " + max);
 	}
 }
