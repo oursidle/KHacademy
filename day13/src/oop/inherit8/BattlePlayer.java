9bf202e619897c635580d07f3d57252b318583d7
ourskim <ourskim@naver.com>
2023-07-05 18:19:14 +0900

    문제

---
+++ day13/src/oop/inherit8/BattlePlayer.java
@@ -0,0 +1,7 @@
+package oop.inherit8;
+
+public class BattlePlayer {
+	BattlePlayer warrior = new BattlePlayer();
+	BattlePlayer Magician = new BattlePlayer();
+	BattlePlayer Archer = new BattlePlayer();
+}
