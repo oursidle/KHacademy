0fc50aa757fec25000633e7cf59da576e52a661a
ourskim <ourskim@naver.com>
2023-07-10 14:04:44 +0900

    생성자가 없는 클래스

---
+++ day16/src/api/lang/etc/Test01.java
@@ -0,0 +1,22 @@
+package api.lang.etc;
+
+public class Test01 {
+	public static void main(String[] args) {
+		//생성자가 없는 클래스
+		//= 클래스는 객체 생성이 주 목적
+		//= 생성자가 없다는 것은 생성자 private이라는 것
+		//= 생성자가 없다는 것은 객체를 만들 수 없다는 의미
+		
+		//[1] 객체를 아예 만들지 못 하게 하려는 목적
+		//= 모든 구성 요소가 전부 static이어야 함
+		//[2] 객체를 만들어주는 메소드를 제공하려는 목적(통제)
+		//= 구성 요소 중 static이 아닌 것이 있는 경우
+		
+		//Math a = new Math();
+		//System b = new System();
+		//Runtime c = Runtime();
+		//Integer d = new Integer(10);
+		
+		
+	}
+}
