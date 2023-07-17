63501bd8f1834974e28c10a0efd550521bdd136d
ourskim <ourskim@naver.com>
2023-07-17 10:20:23 +0900

    파일 복사 프로그램

--- day20/src/api/file/single2/Test03_1.java
+++ day20/src/api/file/single2/Test03_1.java
@@ -4,18 +4,19 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.text.DecimalFormat;
+import java.util.Arrays;
 
-public class Test03 {
+public class Test03_1 {
 	public static void main(String[] args) throws IOException {
 		//파일 복사 프로그램(바이트 배열 - 버퍼 사용)
 		
 		//배열 준비
 		byte[] buffer = new byte[10];
 		
-		//대상 준비
-		File readTarget = new File("D:/origin.txt");
-		File writeTarget = new File("sample/copy2.txt");
+		//대상 준비(운영체제가 거부하는 위치인 경우)
+		String home = System.getProperty("user.home");
+		File readTarget = new File(home, "sample/origin.zip");
+		File writeTarget = new File(home, "sample/copy.zip");
 		
 		//스트림 준비
 		FileInputStream readStream = new FileInputStream(readTarget);
@@ -24,17 +25,10 @@ public class Test03 {
 		//[읽을 파일] --> readTarget --> readStream --> [프로그램]
 		//						  writeStream --> writeTarget --> [내보낼 파일]
 		
-		long count = 0L;//옮긴 글자수
-		long total = readTarget.length();//전체 옮길 글자수
-		DecimalFormat fmt = new DecimalFormat("#.##0.00");
-		
 		while(true) {
 			int size = readStream.read(buffer);
 			if(size == -1) break;
 			writeStream.write(buffer, 0, size);
-			count += size;
-			double percent = count * 100.0 / total;
-			System.out.println("[" + fmt.format(percent) + "%] " + count + " / " + total);
 		}
 
 		//파일 통로 정리
--- day20/src/api/file/single2/Test03_2.java
+++ day20/src/api/file/single2/Test03_2.java
@@ -4,14 +4,14 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.text.DecimalFormat;
+import java.util.Arrays;
 
-public class Test03 {
+public class Test03_2 {
 	public static void main(String[] args) throws IOException {
 		//파일 복사 프로그램(바이트 배열 - 버퍼 사용)
 		
 		//배열 준비
-		byte[] buffer = new byte[10];
+		byte[] buffer = new byte[8192];
 		
 		//대상 준비
 		File readTarget = new File("D:/origin.txt");
@@ -24,18 +24,17 @@ public class Test03 {
 		//[읽을 파일] --> readTarget --> readStream --> [프로그램]
 		//						  writeStream --> writeTarget --> [내보낼 파일]
 		
-		long count = 0L;//옮긴 글자수
-		long total = readTarget.length();//전체 옮길 글자수
-		DecimalFormat fmt = new DecimalFormat("#.##0.00");
+		long start = System.currentTimeMillis();
 		
 		while(true) {
 			int size = readStream.read(buffer);
 			if(size == -1) break;
 			writeStream.write(buffer, 0, size);
-			count += size;
-			double percent = count * 100.0 / total;
-			System.out.println("[" + fmt.format(percent) + "%] " + count + " / " + total);
 		}
+		
+		long finish = System.currentTimeMillis();
+		long duration = finish - start;
+		System.out.println("소요시간 = " + duration + "ms");
 
 		//파일 통로 정리
 		readStream.close();
