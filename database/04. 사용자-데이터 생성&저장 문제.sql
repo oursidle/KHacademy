0a4b9753afc20fe2ff3ed740a8e6b6309cc7ed41
oursidle <135007715+oursidle@users.noreply.github.com>
2023-07-19 10:05:48 +0900

    Update 04. 사용자-데이터 생성&저장 문제.sql

--- database/04. 사용자-데이터 생성&저장 문제.sql
+++ database/04. 사용자-데이터 생성&저장 문제.sql
@@ -1,11 +1,12 @@
 -- 1번 문제
+DROP table
 CREATE TABLE SCORE(
-grade NUMBER,
-class NUMBER,
-name varchar(21),
-korean NUMBER,
-english NUMBER,
-math NUMBER
+grade NUMBER(1),
+room NUMBER,
+name varchar2(21),
+korean NUMBER(3),
+english NUMBER(3),
+math NUMBER(3)
 );
 
 INSERT INTO score(grade, class, name, korean, english, math)
@@ -20,19 +21,22 @@ VALUES(1, 2, '요시', 80, 85, 35);
 SELECT * FROM score;
 
 -- 2번 문제
+-- 이름 작성 시 예약어에 주의
+-- 테이블 이름을 앞에 작성하거나, 이름을 '_'로 길게 작성
+DROP TABLE account;
 CREATE TABLE account(
-name varchar2(21),
-period number,
-round number,
-paid number,
-balance number
+account_owner varchar2(21),
+account_period number,
+account_round number,
+account_payment number,
+account_balance number
 );
 
-INSERT INTO account(name, period, round, paid, balance)
+INSERT INTO account(account_owner, account_period, account_round, account_payment, account_balance)
 values('유재석', 3, 15, 100, 1500);
-INSERT INTO account(name, period, round, paid, balance)
+INSERT INTO account(account_owner, account_period, account_round, account_payment, account_balance)
 values('강호동', 2, 5, 50, 250);
-INSERT INTO account(name, period, round, paid, balance)
+INSERT INTO account(account_owner, account_period, account_round, account_payment, account_balance)
 values('신동엽', 2, 8, 80, 1000);
 
 SELECT * FROM account;
