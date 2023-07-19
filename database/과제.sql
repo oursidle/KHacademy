805bbae5c031c582882fd3a947ce1535c6abdc56
oursidle <135007715+oursidle@users.noreply.github.com>
2023-07-19 19:36:59 +0900

    Update 과제.sql

--- database/과제.sql
+++ database/과제.sql
@@ -7,7 +7,7 @@ book_publication_date char(10) unique,
 book_price number not null,
 book_publisher varchar2(90) unique,
 book_page_count number not null,
-book_genre varchar2(27) not null unique,
+book_genre varchar2(27) not null,
 
 check(regexp_like(book_publication_date, '^(19[0-9]{2}|20[0-9]{2})-(((0[13578]|1[02])-(0[1-9]|1[0-9]|2[0-9]|3[01]))|((0[469]|11)-(0[1-9]|1[0-9]|2[0-9]|30))|((02)-(0[1-9]|1[0-9]|2[0-9])))$')),
 check(book_price >= 0 and book_price <= 9999999.99),
