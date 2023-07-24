3d74302935c700eb4f0d3f69627c8d42d15f290d
ourskim <ourskim@naver.com>
2023-07-24 15:13:56 +0900

    book 테이블 조회

---
+++ jdbc02/src/jdbc/dto/BookDto.java
@@ -0,0 +1,74 @@
+package jdbc.dto;
+
+import java.sql.Date;
+
+public class BookDto {
+	private int bookId;
+	private String bookTitle;
+	private String bookAuthor;
+	private Date bookPublicationDate;
+	private int bookPrice;
+	private String bookPublisher;
+	private int BookPageCount;
+	private String bookGenre;
+	
+	public BookDto() {
+		super();
+	}
+
+	public int getBookId() {
+		return bookId;
+	}
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+	public String getBookTitle() {
+		return bookTitle;
+	}
+	public void setBookTitle(String bookTitle) {
+		this.bookTitle = bookTitle;
+	}
+	public String getBookAuthor() {
+		return bookAuthor;
+	}
+	public void setBookAuthor(String bookAuthor) {
+		this.bookAuthor = bookAuthor;
+	}
+	public Date getBookPublicationDate() {
+		return bookPublicationDate;
+	}
+	public void setBookPublicationDate(Date bookPublicationDate) {
+		this.bookPublicationDate = bookPublicationDate;
+	}
+	public int getBookPrice() {
+		return bookPrice;
+	}
+	public void setBookPrice(int bookPrice) {
+		this.bookPrice = bookPrice;
+	}
+	public String getBookPublisher() {
+		return bookPublisher;
+	}
+	public void setBookPublisher(String bookPublisher) {
+		this.bookPublisher = bookPublisher;
+	}
+	public int getBookPageCount() {
+		return BookPageCount;
+	}
+	public void setBookPageCount(int BookPageCount) {
+		this.BookPageCount = BookPageCount;
+	}
+	public String getBookGenre() {
+		return bookGenre;
+	}
+	public void setBookGenre(String bookGenre) {
+		this.bookGenre = bookGenre;
+	}
+
+	@Override
+	public String toString() {
+		return "BookDto [bookId=" + bookId + ", bookTitle=" + bookTitle + ", bookAuthor=" + bookAuthor
+				+ ", bookPublicationDate=" + bookPublicationDate + ", bookPrice=" + bookPrice + ", bookPublisher="
+				+ bookPublisher + ", BookPageCount=" + BookPageCount + ", bookGenre=" + bookGenre + "]";
+	}
+}
---
+++ jdbc02/src/jdbc/select/Test04.java
@@ -0,0 +1,26 @@
+package jdbc.select;
+
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import jdbc.dto.BookDto;
+import jdbc.mapper.BookMapper;
+import jdbc02.JdbcUtils;
+
+public class Test04 {
+	public static void main(String[] args) {
+		
+		String sql = "select * from book order by book_id desc";
+		
+		BookMapper mapper = new BookMapper();
+		JdbcTemplate jdbcTemplate =JdbcUtils.getJdbcTemplate();
+		List<BookDto> list = jdbcTemplate.query(sql, mapper);
+		
+		System.out.println("조회수: " + list.size());
+		for(BookDto dto : list) {
+			System.out.println(dto);
+		}
+	}
+
+}
