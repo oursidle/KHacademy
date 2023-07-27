1ecaf97ec6cadd6955245c0bdc8f6baaac77fd63
ourskim <ourskim@naver.com>
2023-07-27 17:56:51 +0900

    포켓몬스터 목록 삭제 & 조회

--- spring05jdbc/src/main/java/com/kh/spring05/dao/PocketmonDao.java
+++ spring05jdbc/src/main/java/com/kh/spring05/dao/PocketmonDao.java
@@ -1,11 +1,13 @@
 package com.kh.spring05.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import com.fasterxml.jackson.core.sym.Name;
 import com.kh.spring05.dto.PocketmonDto;
+import com.kh.spring05.mapper.PocketmonMapper;
 
 //DAO 클래스
 //= Spring에 등록해야 함
@@ -13,8 +15,12 @@ import com.kh.spring05.dto.PocketmonDto;
 //= @Repository로 등록(영원히 변하지 않는 것을 다루는 객체. 영속성 제어 객체)
 @Repository
 public class PocketmonDao {
+	
 	@Autowired//(주의) 등록을 하지 않으면 절대로 주지 않음
 	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	private PocketmonMapper mapper;
 	
 	public void insert(PocketmonDto dto) {
 		String sql = "insert into pocketmon(no, name, type)"
@@ -32,5 +38,16 @@ public class PocketmonDao {
 		Object[] data = {dto.getName(), dto.getType(), dto.getNo()};
 		
 		return jdbcTemplate.update(sql, data) > 0;
+	}
+
+	public boolean delete(int no) {
+		String sql = "delete pocketmon where no = ?";
+		Object[] data = {no};
+		return jdbcTemplate.update(sql, data) > 0;
+	}
+	
+	public List<PocketmonDto> selectList(){
+		String sql = "select * from pocketmon order by no asc";
+		return jdbcTemplate.query(sql, mapper);
 	}
 }
