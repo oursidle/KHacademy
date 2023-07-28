a1d266129f40a6abebf2c6d32aa47b4336170885
ourskim <ourskim@naver.com>
2023-07-28 09:49:37 +0900

    포켓몬스터 상세 조회

--- spring05jdbc/src/main/java/com/kh/spring05/dao/PocketmonDao.java
+++ spring05jdbc/src/main/java/com/kh/spring05/dao/PocketmonDao.java
@@ -50,4 +50,11 @@ public class PocketmonDao {
 		String sql = "select * from pocketmon order by no asc";
 		return jdbcTemplate.query(sql, mapper);
 	}
+	
+	public PocketmonDto selectOne(int no) {
+		String sql = "select * from pocketmon where no = ?";
+		Object[] data = {no};
+		List<PocketmonDto> list = jdbcTemplate.query(sql, mapper, data);
+		return list.isEmpty() ? null : list.get(0);
+	}
 }
