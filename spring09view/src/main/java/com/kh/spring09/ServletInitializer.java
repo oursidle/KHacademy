a39a452a7b72a034e0e5692236c126e3d1de3687
oursidle <ourskim722@naver.com>
2023-07-31 10:04:41 +0900

    spring09

--- spring09view/src/main/java/com/kh/spring09/ServletInitializer.java
+++ spring09view/src/main/java/com/kh/spring09/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.kh.spring09;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
--- spring09view/src/main/java/com/kh/spring09/Spring09viewApplication.java
+++ spring09view/src/main/java/com/kh/spring09/Spring09viewApplication.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.kh.spring09;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
