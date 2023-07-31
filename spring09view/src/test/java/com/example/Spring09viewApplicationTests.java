bfd29e816bc604588a12f1169f756c3371625589
oursidle <ourskim722@naver.com>
2023-07-31 10:03:10 +0900

    spring09

---
+++ spring09view/src/test/java/com/example/Spring09viewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring09viewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
