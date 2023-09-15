0da13e9e529b24b0251b2afb3c456e04ad4eea6e
oursidle <oursidle@naver.com>
2023-09-15 10:56:44 +0900

    파일 경로 커스터마이징

--- spring12file/src/main/java/com/kh/spring12/controller/FileRestController.java
+++ spring12file/src/main/java/com/kh/spring12/controller/FileRestController.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
+
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.kh.spring12.configuration.FileUploadProperties;
 import com.kh.spring12.dao.AttachDao;
 import com.kh.spring12.dto.AttachDto;
 
@@ -30,6 +33,18 @@ public class FileRestController {
 	@Autowired
 	private AttachDao attachDao;
 	
+	//미리 작성해둔 커스텀 속성을 불러와 디렉토리 객체까지 생성
+	@Autowired
+	private FileUploadProperties props;
+	
+	private File dir;
+	
+	@PostConstruct//모든 로딩이 끝나면 자동으로 실행되는 메소드
+	public void init() {
+		dir = new File(props.getHome());
+		dir.mkdirs();
+	}
+	
 	//비동기 통신에서는 화면에서 다음 작업이 가능하도록 파일번호 등을
 	@PostMapping("/upload")
 	public Map<String, Object> upload(@RequestParam MultipartFile attach) throws IllegalStateException, IOException{
@@ -39,9 +54,9 @@ public class FileRestController {
 		int attachNo = attachDao.sequence();
 		
 		//[2] 같은 이름에 대한 충돌을 방지하기 위해 
-		String home = System.getProperty("user.home");
-		File dir = new File(home, "upload");//저장할 디렉토리
-		dir.mkdirs();//디렉토리 생성
+//		String home = System.getProperty("user.home");
+//		File dir = new File(home, "upload");//저장할 디렉토리
+//		dir.mkdirs();//디렉토리 생성
 		
 		File target = new File(dir, String.valueOf(attachNo));//저장할 파일 객체
 		attach.transferTo(target);//저장
@@ -69,8 +84,8 @@ public class FileRestController {
 			return ResponseEntity.notFound().build();
 		}
 		
-		String home = System.getProperty("user.home");
-		File dir = new File(home, "upload");
+//		String home = System.getProperty("user.home");
+//		File dir = new File(home, "upload");
 		File target = new File(dir, String.valueOf(attachDto.getAttachNo()));
 		
 		byte[] data = FileUtils.readFileToByteArray(target);
