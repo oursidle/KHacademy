56ae6c61543f211aefba28cc425489ad527d36c2
oursidle <oursidle@naver.com>
2023-10-12 16:10:41 +0900

    book dto,mapper

---
+++ spring15book/src/main/java/com/kh/spring15/dto/BookDto.java
@@ -0,0 +1,16 @@
+package com.kh.spring15.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class BookDto {
+	private int bookId;
+	private String bookTitle, bookAuthor, bookPublicationDate;
+	private int bookPrice;
+	private String bookPublisher;
+	private int bookPageCount;
+	private String bookGenre;
+}
