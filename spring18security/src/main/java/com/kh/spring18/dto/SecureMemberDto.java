1227e8f26ae71bf8dab0adda42e8001aafa035c0
oursidle <oursidle@naver.com>
2023-10-16 16:18:12 +0900

    회원가입 (+암호화)

---
+++ spring18security/src/main/java/com/kh/spring18/dto/SecureMemberDto.java
@@ -0,0 +1,12 @@
+package com.kh.spring18.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class SecureMemberDto {
+	private String memberId;
+	private String memberPw;
+}
