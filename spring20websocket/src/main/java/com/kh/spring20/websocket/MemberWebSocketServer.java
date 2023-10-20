9c829bc4c1b25ea448476024e1d2552be463b49a
oursidle <oursidle@naver.com>
2023-10-20 16:03:21 +0900

    회원 채팅에 아아디 정보 추가

--- spring20websocket/src/main/java/com/kh/spring20/websocket/MemberWebSocketServer.java
+++ spring20websocket/src/main/java/com/kh/spring20/websocket/MemberWebSocketServer.java
@@ -49,8 +49,11 @@ public class MemberWebSocketServer extends TextWebSocketHandler {
 			return;
 		}
 		
+		//메세지에 송신자의 ID를 추가하여 전송
+		TextMessage tm = new TextMessage("[" + memberId + "] " + message.getPayload());
+		
 		for(WebSocketSession clients : clients) {
-			clients.sendMessage(message);
+			clients.sendMessage(tm);
 		}
 	}
 }
