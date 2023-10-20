4c1544d7c0be800dd607a1484e2958aef3058813
oursidle <oursidle@naver.com>
2023-10-20 13:58:42 +0900

    웹소켓 설정

---
+++ spring20websocket/src/main/java/com/kh/spring20/configuration/WebSocketServerConfiguration.java
@@ -0,0 +1,35 @@
+package com.kh.spring20.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import com.kh.spring20.websocket.DefaultWebSocketServer;
+import com.kh.spring20.websocket.GroupWebSocketServer;
+import com.kh.spring20.websocket.TimeWebSocketServer;
+
+//이 클래스는 생성한 웹소켓 서버를 어떤 주소에 할당하도록 설정하는 역할
+@EnableWebSocket
+@Configuration
+public class WebSocketServerConfiguration implements WebSocketConfigurer{
+	
+	@Autowired
+	private DefaultWebSocketServer defaultWebSocketServer;
+	
+	@Autowired
+	private TimeWebSocketServer timeWebSocketServer;
+	
+	@Autowired
+	private GroupWebSocketServer groupWebSocketServer;
+	
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		//등록할 때는 주소와 도구를 연결해야 함(필요하다면 추가 옵션 설정)
+		//(주의) 절대로 화면의 주소와 겹치면 안됨
+		registry.addHandler(defaultWebSocketServer, "/ws/default");
+		registry.addHandler(timeWebSocketServer, "/ws/time");
+		registry.addHandler(groupWebSocketServer, "/ws/group");
+	}
+}
---
+++ spring20websocket/src/main/java/com/kh/spring20/controller/WebSocketViewController.java
@@ -0,0 +1,32 @@
+package com.kh.spring20.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebSocketViewController {
+	
+	@RequestMapping("/")
+	public String home() {
+		//return "/WEB-INF/views/home.jsp";
+		return "home";
+	}
+	
+	@RequestMapping("/default")
+	public String defaultServer() {
+		//return "/WEB-INF/views/default.jsp";
+		return "default";
+	}
+	
+	@RequestMapping("/time")
+	public String timeServer() {
+//		return "/WEB-INF/views/time.jsp";
+		return "time";
+	}
+	
+	@RequestMapping("/group")
+	public String group() {
+//		return "/WEB-INF/views/group.jsp";
+		return "group";
+	}
+}
